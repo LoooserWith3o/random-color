d6319817728dabb7466dda6916edfb9ff34f96ac
Chris <christiantokic@hotmail.com>
2022-05-05 02:20:37 +0200

    Modified index.js and package.json, added yarn.lock

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
+// Import Dependancies
+
 import chalk from 'chalk';
 import randomColor from 'randomcolor';
 
+// Console Output if no specific input
 const randomCol = '#' + Math.floor(Math.random() * 16777215).toString(16);
-const block = `               ################################
-               ################################
-               ####                        ####
-               ####    ${randomCol};            ####
-               ####                        ####
-               ####                        ####
-               ################################
-               ################################`;
+const block = `################################
+################################
+####                        ####
+####         ${randomCol}        ####
+####                        ####
+####                        ####
+################################
+################################`;
+// If input is provided
 if (process.argv < 2) {
   console.log(chalk.hex(randomCol).bold(block));
 } else {
